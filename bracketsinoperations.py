003d4e32043a03c0db002fce776c7ae2d1690327
Beryl Chebet <berylchebet44@gmail.com>
2021-11-08 22:37:29 +0300

    Add files via upload

---
+++ bracketsinoperations.py
@@ -0,0 +1,9 @@
+
+miles = 7.38 #declaring values of kilometers and meters.
+kilometers = 12.25
+
+miles_to_kilometers = miles *1.61 #converting miles to kilometers
+kilometers_to_miles = kilometers/1.61 # converting kilometers to miles
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
